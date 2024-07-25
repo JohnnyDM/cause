2b966c6fde5de2e1c5b11abacf15bc0eea0fcf99
JohnnyDM <johnny.gras97@gmail.com>
2024-07-25 14:06:10 +0200

    Skill-Tab V2 functionality
    -Added Skilllevel
    -Added Colorchanging
    -Added Profs
    - Added Delete/Edit

--- module/sheets/item-sheet.mjs
+++ module/sheets/item-sheet.mjs
@@ -1,5 +1,40 @@
 export default class CauseItemSheet extends ItemSheet {
+    static get defaultOptions() {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+          classes: ['cause', 'sheet', 'item'],
+          width: 520,
+          height: 480,
+          tabs: [
+            {
+              navSelector: '.sheet-tabs',
+              contentSelector: '.sheet-body',
+              initial: 'description',
+            },
+          ],
+        });
+      }
+    
     get template() {
         return `systems/cause/templates/item/item-${this.item.type}.hbs`;
     }
+
+    getData() {
+        // Retrieve base data structure.
+        const context = super.getData();
+      
+        // Use a safe clone of the item data for further operations.
+        const itemData = context.data;
+      
+        // Retrieve the roll data for TinyMCE editors.
+        context.rollData = this.item.getRollData();
+      
+        // Add the item's data to context.data for easier access, as well as flags.
+        context.system = itemData.system;
+        context.flags = itemData.flags;
+      
+        // Prepare active effects for easier access
+        //context.effects = prepareActiveEffectCategories(this.item.effects);
+      
+        return context;
+      }
 }
